f5085fc8a086332b21d0f7b5b2e64107e727b2f8
kofllee <111866132+kofllee@users.noreply.github.com>
2026-05-07 18:10:04 +0300

    fix: update mod version to 1.0.1 and handle null config screen factory

--- src/client/java/net/kofllee/hoverhints/client/config/HoverHintsModMenuIntegration.java
+++ src/client/java/net/kofllee/hoverhints/client/config/HoverHintsModMenuIntegration.java
@@ -2,13 +2,14 @@ package net.kofllee.hoverhints.client.config;
 
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
+import com.terraformersmc.modmenu.util.NullScreenFactory;
 
 public final class HoverHintsModMenuIntegration implements ModMenuApi {
 
     @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
         if (!HoverHintsConfigDependencies.hasClothConfig()) {
-            return null;
+            return new NullScreenFactory<>();
         }
 
         return HoverHintsConfigScreen::create;
